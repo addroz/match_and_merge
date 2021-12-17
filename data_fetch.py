252b4f76b7e3d35a109607ffb4edbe5c6ffbad33
Adam Drozynski <adam@vespertool.com>
2021-12-17 14:51:08 +0100

    moved links to the config

--- data_fetch.py
+++ data_fetch.py
@@ -3,16 +3,13 @@ import zipfile
 
 import pandas as pd
 import requests
+import config
 
-WRI_DB_LINK = "https://wri-dataportal-prod.s3.amazonaws.com/manual/global_power_plant_database_v_1_3.zip?download=1"
-JRC_DB_LINK = "https://zenodo.org/record/3574566/files/JRC-PPDB-OPEN.ver1.0.zip?download=1"
-
-
-wri_db = requests.get(WRI_DB_LINK)
+wri_db = requests.get(config.WRI_DB_LINK)
 wri_db_unpacked = zipfile.ZipFile(io.BytesIO(wri_db.content))
 wri_db_unpacked.extractall("./wri_db_original")
 
-jrc_db = requests.get(JRC_DB_LINK)
+jrc_db = requests.get(config.JRC_DB_LINK)
 jrc_db_unpacked = zipfile.ZipFile(io.BytesIO(jrc_db.content))
 jrc_db_unpacked.extractall("./jrc_db_original")
 
